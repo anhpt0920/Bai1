523f4edfd3bb6e64e0b0a1511e54e99bf3dd6e76
anhpt0920 <panhtokusatsu@gmail.com>
2024-11-19 17:50:06 +0700

    Update Exchange.java

--- Chuong 5/Bai 2/Exchange.java
+++ Chuong 5/Bai 2/Exchange.java
@@ -1,17 +1,16 @@
 package bai2;
-import jva.util.ArrayList;
+import java.util.ArrayList;
 class Exchange {
     ArrayList<Rate> rates;
-
     public Exchange() {
         rates = new ArrayList<>();
     }
-    // Phương thức thêm tỷ giá vào danh sách
+    //Phương thức thêm tỷ giá vào danh sách
     public void addRate(String code1, String code2, double rate) {
         rates.add(new Rate(code1, code2, rate));
     }
 
-    // Phương thức chuyển đổi
+    //Phương thức chuyển đổi
     public double convert(String code1, String code2, double amount) {
         for (Rate rate : rates) {
             if (rate.code1.equals(code1) && rate.code2.equals(code2)) {
@@ -19,6 +18,6 @@ class Exchange {
             }
         }
         System.out.println("Không tìm thấy tỷ giá cho mã tiền tệ " + code1 + " -> " + code2);
-        return -1; // Trả về -1 nếu không tìm thấy tỷ giá
+        return -1; //Trả về -1 nếu không tìm thấy tỷ giá
     }
 }
